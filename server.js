bb8d9b95e99449357f77e2ea6da9c0358cbb1a5d
Asel Atakeev <aselya.atakeeva@gmail.com>
2021-11-04 13:29:11 -0700

    added path and fs

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = 3000;
 const path = require('path');
 const fs = require('fs');
 
-
 const server = http.createServer((req, res) => {
     console.log(`Request for ${req.url} by method ${req.method}`);
 
@@ -43,3 +42,8 @@ const server = http.createServer((req, res) => {
         res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
     }
 });
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
